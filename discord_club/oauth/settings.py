bb2d5b458f0be0a1d39390eb4fc5f6e23905636a
Merlintor <merlinfuchs2001@gmail.com>
2019-12-24 15:56:00 +0100

    Deploy in docker container

--- discord_club/oauth/settings.py
+++ discord_club/oauth/settings.py
@@ -1,4 +1,6 @@
+from os import environ as env
+
 CLIENT_ID = 465946982339444746
-CLIENT_SECRET = ""
+CLIENT_SECRET = env.get("CLIENT_SECRET")
 SCOPES = ["identify"]
 REDIRECT_URI = "https://discord.club/oauth/callback/"
